6c257293d9b4e184f704f81b03728df1b9e53462
ajvernon <a.vernon@outlook.com>
2017-10-24 12:57:35 +0100

    Added templates

--- forum/app.py
+++ forum/app.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
